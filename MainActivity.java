f597a9c8a415d8ca204727e6fced162d3179069d
ttt4tsuo <ttt4tsuo@gmail.com>
2018-08-06 02:17:06 +0900

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -61,31 +61,97 @@ public class MainActivity extends AppCompatActivity {
 
             WebView wv= (WebView)findViewById(R.id.web_view);
             wv.getSettings().setJavaScriptEnabled(true);
+            wv.getSettings().setAppCacheEnabled(true);
             wv.setWebViewClient(new WebViewClient() {
-                public void doUpdateVisitedHistory(WebView view, String url, boolean isReload){}
-                public void onFormResubmission(WebView view, Message dontResend, Message resend) {}
-                public void onLoadResource(WebView view, String url){}
-                public void onPageCommitVisible(WebView view, String url){}
-                public void onPageFinished(WebView view, String url){}
-                public void onPageStarted(WebView view, String url, Bitmap favicon){}
-                public void onReceivedClientCertRequest(WebView view, ClientCertRequest request){}
-
-                public void onReceivedError(WebView view, int errorCode, String description, String failingUrl){}
-                public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error){}
-                public void onReceivedHttpAuthRequest(WebView view, HttpAuthHandler handler, String host, String realm){}
-                public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse){}
-                public void onReceivedLoginRequest(WebView view, String realm, String account, String args){}
-                public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error){}
-                public boolean onRenderProcessGone(WebView view, RenderProcessGoneDetail detail){return true;}
+                @Override
+                public void doUpdateVisitedHistory(WebView view, String url, boolean isReload){
+                    super.doUpdateVisitedHistory(view,url,isReload);
+                }
+                @Override
+                public void onFormResubmission(WebView view, Message dontResend, Message resend) {
+                    super.onFormResubmission(view,dontResend,resend);
+                }
+                @Override
+                public void onLoadResource(WebView view, String url){
+                    super.onLoadResource(view,url);
+                }
+                @Override
+                public void onPageCommitVisible(WebView view, String url){
+                    super.onPageCommitVisible(view,url);
+                }
+                @Override
+                public void onPageFinished(WebView view, String url){
+                    super.onPageFinished(view,url);
+                }
+                @Override
+                public void onPageStarted(WebView view, String url, Bitmap favicon){
+                    super.onPageStarted(view,url,favicon);
+                }
+                @Override
+                public void onReceivedClientCertRequest(WebView view, ClientCertRequest request){
+                    super.onReceivedClientCertRequest(view,request);
+                }
+                @Override
+                public void onReceivedError(WebView view, int errorCode, String description, String failingUrl){
+                    super.onReceivedError(view,errorCode,description,failingUrl);
+                }
+                @Override
+                public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error){
+                    super.onReceivedError(view,request,error);
+                }
+                @Override
+                public void onReceivedHttpAuthRequest(WebView view, HttpAuthHandler handler, String host, String realm){
+                    super.onReceivedHttpAuthRequest(view,handler,host,realm);
+                }
+                @Override
+                public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse){
+                    super.onReceivedHttpError(view,request,errorResponse);
+                }
+                @Override
+                public void onReceivedLoginRequest(WebView view, String realm, String account, String args){
+                    super.onReceivedLoginRequest(view,realm,account,args);
+                }
+                @Override
+                public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error){
+                    super.onReceivedSslError(view,handler,error);
+                }
+                @Override
+                public boolean onRenderProcessGone(WebView view, RenderProcessGoneDetail detail){
+                    return super.onRenderProcessGone(view,detail);
+                }
                 //public void onSafeBrowsingHit(WebView view, WebResourceRequest request, int threatType, SafeBrowsingResponse callback){}
-                public void onScaleChanged(WebView view, float oldScale, float newScale){}
-                public void onTooManyRedirects(WebView view, Message cancelMsg, Message continueMsg){}
-                public void onUnhandledKeyEvent(WebView view, KeyEvent event){}
-                public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request){return null;}
-                public WebResourceResponse shouldInterceptRequest(WebView view, String url){return null;}
-                public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event){return true;}
-                public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request){return true;}
-                public boolean shouldOverrideUrlLoading(WebView view, String url){return true;}
+                @Override
+                public void onScaleChanged(WebView view, float oldScale, float newScale){
+                    super.onScaleChanged(view,oldScale,newScale);
+                }
+                @Override
+                public void onTooManyRedirects(WebView view, Message cancelMsg, Message continueMsg){
+                    super.onTooManyRedirects(view,cancelMsg,continueMsg);
+                }
+                @Override
+                public void onUnhandledKeyEvent(WebView view, KeyEvent event){
+                    super.onUnhandledKeyEvent(view,event);
+                }
+                @Override
+                public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request){
+                    return super.shouldInterceptRequest(view,request);
+                }
+                @Override
+                public WebResourceResponse shouldInterceptRequest(WebView view, String url){
+                    return super.shouldInterceptRequest(view,url);
+                }
+                @Override
+                public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event){
+                    return super.shouldOverrideKeyEvent(view,event);
+                }
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request){
+                    return super.shouldOverrideUrlLoading(view,request);
+                }
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url){
+                    return super.shouldOverrideUrlLoading(view,url);
+                }
 
             });
             wv.setWebChromeClient(new WebChromeClient() {
